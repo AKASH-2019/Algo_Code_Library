c718d51dfc78696daf40a8485894429c075f960c
Munazer Montasir <50144800+AKASH-2019@users.noreply.github.com>
2020-08-07 21:08:29 +0600

    Add files via upload

---
+++ tamplate/vector_pair_sort.cpp
@@ -0,0 +1,38 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+bool compare(pair<int,int>a,pair<int,int>b)
+{
+	return a.second<b.second;
+}
+
+int main()
+{
+    vector< pair <int,int> > vect;
+    int n,x,y;
+    cin>>n;
+
+    for (int i=0; i<n; i++)
+    {
+        cin>>x>>y;
+        vect.push_back( make_pair(x,y) );
+    }
+    
+    sort(vect.begin(),vect.end(),compare);
+    
+    for(int i=0;i<n;i++)
+    {
+		cout<<vect[i].first<<" "<<vect[i].second<<endl;
+	}
+ 
+
+    return 0;
+}
+
+/**
+ 4
+ 1 10
+ 2 8
+ 3 5
+ 4 1
+ */
