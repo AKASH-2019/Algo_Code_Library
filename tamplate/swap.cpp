d4d5d687c4266515ea24d8a44242bb34c919cea1
Munazer Montasir <50144800+AKASH-2019@users.noreply.github.com>
2020-11-18 21:07:20 +0600

    Add files via upload

---
+++ tamplate/swap.cpp
@@ -0,0 +1,50 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+
+
+
+// swap ...................
+
+void main()
+{
+	// xor
+    int x = 6, y = 4;
+    x = x^y;     
+    y = x^y;
+    x = x^y;
+    printf("x = %d and y = %d", x, y);
+    getch();
+
+
+   // addition
+   int X= 25 , Y= 23 ;  
+   X = X + Y = 25 +23 = 48;
+   Y = X - Y = 48 - 23 = 25;
+   X = X -Y = 48 - 25 = 23;
+   printf("x = %d and y = %d",X,Y);
+
+
+    int x = 10, y = 15;
+    x = x + y - (y = x);
+    printf("x = %d and y = %d",x,y);
+    
+    
+    // multiplication
+    int x = 6, y = 4;
+    x = x*y;
+    y = x/y;
+    x = x/y;
+    printf("x = %d and y = %d", x, y);
+}
+
+ 
+
+
+
+
+
+
+
+
